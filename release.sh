c594de615b26b089e17617f5515d997f6949548d
Nathan Leiby <nathanleiby@gmail.com>
2024-07-10 17:26:47 -0700

    v0.0.11

--- release.sh
+++ release.sh
@@ -1,13 +1,34 @@
 # !/bin/bash
 
+# prep
 rustup target add x86_64-apple-darwin
+rustup target add aarch64-apple-darwin
+
+mkdir -p release
+
+
+# build: x86
+target=x86_64-apple-darwin
+final_binary_name=macroix-x86
+cargo build --release --target $target
+cp target/x86_64-apple-darwin/release/macroix ./release/$final_binary_name
+chmod 700 ./release/$final_binary_name
+
+# build: arm
+cargo build --release --target aarch64-apple-darwin
+cp target/aarch64-apple-darwin/release/macroix ./release/macroix-aarch64
+chmod 700 ./release/macroix-aarch64
+
+# build: universal (TBD: not working)
 cargo install universal2
 cargo universal2
-mkdir -p release
-cp target/universal2-apple-darwin/macroix ./release/macroix
-chmod 700 ./release/macroix
+cp target/universal2-apple-darwin/macroix ./release/macroix-universal
+chmod 700 ./release/macroix-universal
+
+# add assets
 cp -r res/ ./release/res/
 
+# zip it up and ship the release
 version="$(cat VERSION)"
 zip_file=release-$version.zip
 zip -r $zip_file release
