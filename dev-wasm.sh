e2a2fd065288431ed3f7a80c47541e891e31429b
Nathan Leiby <nathanleiby@gmail.com>
2024-11-08 18:46:14 -0800

    chore: add runner script for dev-wasm

---
+++ dev-wasm.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+# exit on error
+set -e
+
+rustup target add wasm32-unknown-unknown
+./wasm-bindgen-macroquad.sh macroix
+
+## TODO: Figure out how to properly
+# cp -r res dist/
+
+pushd dist/
+basic-http-server -a 0.0.0.0:4001 .
+popd
--- release-wasm.sh
+++ release-wasm.sh
@@ -6,4 +6,7 @@ set -e
 rustup target add wasm32-unknown-unknown
 ./wasm-bindgen-macroquad.sh --release macroix
 rm -rf github_pages
+
+## TODO: Figure out how to properly handle resources, like audio files and images
+# cp -r res dist/
 cp -r dist github_pages
