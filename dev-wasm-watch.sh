d886b8ac596eba9f5fe8e2f4eaa025bd225ab6a1
Nathan Leiby <nathanleiby@gmail.com>
2025-04-18 21:08:40 -0700

    docs: clarify streamline dev workflow

--- dev-wasm-watch.sh
+++ dev-wasm-watch.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 git ls-files |   entr -r timeout -k 5 0 ./dev-wasm.sh
+
+# TODO: it would be nice if clicking "run game" weren't required for faster dev loop.. can i avoid that
+# also -- can plug into "storybook" idea of previewing various UI screens, component mindset
